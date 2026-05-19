03bc512e341f7d0c0946381dc0b2c2e116a1d866
Ubuntu <ubuntu@ip-172-31-27-171.ap-southeast-2.compute.internal>
2026-05-19 15:37:48 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Node.js App Running on EC2 with Docker");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
